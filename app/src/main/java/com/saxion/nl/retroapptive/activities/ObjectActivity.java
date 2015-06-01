d331d2f54751b43b09b4156e3188e902be6c2031
tbotting <thomas-1996@live.nl>
2015-06-01 16:55:13 +0200

    Merge branch 'master' of https://github.com/JonathanBergers/RetroApptive
    Conflicts:
    	app/src/main/java/com/saxion/nl/retroapptive/MainActivity.java

--- app/src/main/java/com/saxion/nl/retroapptive/activities/ObjectActivity.java
+++ app/src/main/java/com/saxion/nl/retroapptive/activities/ObjectActivity.java
@@ -13,36 +13,36 @@ import com.saxion.nl.retroapptive.R;
 /**
  * Created by Jelle on 1-6-2015.
  */
-public class ObjectActivity extends Activity{
+public class ObjectActivity extends Activity {
 
-    private EditText titleEditText, decriptionEditText, categoryEditText;
-    private Button save;
+	private EditText titleEditText, decriptionEditText, categoryEditText;
+	private Button save;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_new_object);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_new_object);
 
-        save = (Button) findViewById(R.id.buttonSave);
+		save = (Button) findViewById(R.id.buttonSave);
 
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //getting de values
-                String title, description, category;
+		save.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//getting de values
+				String title, description, category;
 
-                title = ""+titleEditText.getText();
-                description = "" + decriptionEditText.getText();
-                category = "" + categoryEditText.getText();
+				title = "" + titleEditText.getText();
+				description = "" + decriptionEditText.getText();
+				category = "" + categoryEditText.getText();
 
-                //new object van user story of note
-                //new Object(title, description,category);
-                //waardes title description, catergory
+				//new object van user story of note
+				//new Object(title, description,category);
+				//waardes title description, catergory
 
-                //terug gaan naar de main view
-                Intent i = new Intent(ObjectActivity.this, MainActivity.class);
-                startActivity(i);
-            }
-        });
-    }
+				//terug gaan naar de main view
+				Intent i = new Intent(ObjectActivity.this, MainActivity.class);
+				startActivity(i);
+			}
+		});
+	}
 }
